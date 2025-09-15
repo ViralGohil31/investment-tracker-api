6af41b23353ac9527afec01117c84f7f9a38fc2b
ViralGohil31 <viralgohil31@gmail.com>
2025-09-15 14:09:51 +0530

    Added initial setup

---
+++ src/main/java/com/investment/InvestmentTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.investment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentTrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/investment/controller/InvestmentController.java
@@ -0,0 +1,10 @@
+package com.investment.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/investments")
+public class InvestmentController {
+
+}
