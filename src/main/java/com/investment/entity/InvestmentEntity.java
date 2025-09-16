26e761b8146a36c1cce4cc25450ccba83484dbed
ViralGohil31 <viralgohil31@gmail.com>
2025-09-16 23:13:02 +0530

    Added logging and entity class

--- src/main/java/com/investment/entity/InvestmentEntity.java
+++ src/main/java/com/investment/entity/InvestmentEntity.java
@@ -4,6 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.*;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
@@ -12,5 +16,14 @@ import lombok.*;
 @Entity
 @Table(name = "investments")
 public class InvestmentEntity {
+    private UUID id;
+    private String userId;
+    private String name;
+    private String type;
+    private BigDecimal amount;
+    private String currency;
+    private String purchase_date;
+    private LocalDateTime created_at;
+    private LocalDateTime updated_at;
 
 }
--- src/main/resources/db/changelog/DDL/Create_Investments_Table.sql
+++ src/main/resources/db/changelog/DDL/Create_Investments_Table.sql
@@ -1,5 +1,7 @@
-CREATE TABLE IF NOT EXISTS investments (
-    investment_id   VARCHAR(50) PRIMARY KEY,
+
+DROP TABLE IF EXISTS investments CASCADE;
+CREATE TABLE investments (
+    id              UUID NOT NULL,
     user_id         VARCHAR(50) NOT NULL,
     name            VARCHAR(200) NOT NULL,
     type            VARCHAR(50),               -- Stock, Bond, MutualFund, etc.
@@ -9,6 +11,7 @@ CREATE TABLE IF NOT EXISTS investments (
     created_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at      TIMESTAMP,
 
+    CONSTRAINT pk_investments PRIMARY_KEY (id),
     CONSTRAINT fk_investment_user
       FOREIGN KEY (user_id) REFERENCES users(user_id)
       ON DELETE CASCADE
